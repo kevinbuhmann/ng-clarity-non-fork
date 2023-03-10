03d8072ab58c5dec70d3548902f98e53d2a598b9
Kevin Buhmann <kbuhmann@vmware.com>
2023-03-10 10:12:07 -0600

    chore: add visual regression tests via `playwright`
    Chromatic is not free if you take lots of snapshots.
    
    This is a possible alternative. GitHub Actions time is unlimited for
    public repositories, so this solution is free.

---
+++ playwright.config.ts
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2016-2023 VMware, Inc. All Rights Reserved.
+ * This software is released under MIT license.
+ * The full license information can be found in LICENSE in the root directory of this project.
+ */
+
+import { defineConfig, devices } from '@playwright/test';
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+  testDir: './tests',
+  timeout: 30 * 1000,
+  expect: {
+    timeout: 5000,
+  },
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: 'html',
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+  webServer: {
+    command: 'npm run ts-node -- ./scripts/start-storybook-server.ts',
+    port: 8080,
+  },
+});
